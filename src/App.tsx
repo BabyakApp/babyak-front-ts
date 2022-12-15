bce0da473d24d2dd3d66ef04cbc467dd5cae2fd2
jh-jin <devjin0122@gmail.com>
2022-12-15 16:18:07 +0900

    Feat: put Main Page Image

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import mainImage from "./mainImage.png";
 import Post from "./components/postBox";
 import AllPostList from "./pages/allPostList";
-import {BrowserRouter, Route} from "react-router-dom";
-import GlobalStyles from './style/GlobalStyles';
+import { BrowserRouter, Route } from "react-router-dom";
+import GlobalStyles from "./style/GlobalStyles";
 
 function App() {
   return (
     <div className="App">
-      <GlobalStyles />
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <img className="mainImage" src={mainImage} alt="mainImage" />
     </div>
-    
   );
 }
 
--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import {BrowserRouter, Route, Routes} from "react-router-dom";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import AllPostList from "./pages/allPostList";
 import PostDetail from "./pages/postDetail";
-import {postListTest, noShowEx} from "data/testdata"
+import { postListTest, noShowEx } from "data/testdata";
 import Chatting from "./pages/chatting";
 import MyPage from "./pages/myPage";
 import MajorPostList from "./pages/majorPostList";
@@ -17,30 +17,41 @@ import GetPost from "./data/getPost";
 import Post from "./data/posting";
 import PostList from "./components/postList";
 import PostFilterBar from "./components/postFilterBar";
-import {PostDetailContainer} from "./container/postDetailContainer";
-import {MypageContainer} from "./container/mypageContainer";
+import { PostDetailContainer } from "./container/postDetailContainer";
+import { MypageContainer } from "./container/mypageContainer";
 import NoshowModal from "./pages/noshowModal";
-
+import SignUp from "pages/signUp";
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
 root.render(
-    <BrowserRouter>
-        <Routes>
-            <Route path="/" element={<App />} />
-            <Route path="postlist" element={<PostList posts={postListTest.posts} userid={4} major={"컴공"} />} />
-            <Route path="post" element={<AllPostList />} />
-            <Route path="postdetail" element={<PostDetailContainer />} />
-            <Route path="majorpostlist" element={<MajorPostList major={"컴공"} posts={postListTest.posts} userid={1} />} />
-            <Route path="chatting" element={<Chatting />} />
-            <Route path="mypage" element={<MypageContainer />} />
-            <Route path="posting" element={<Posting />} />
-            <Route path="noshow" element={<NoshowReport  users={noShowEx.users}/>} />
-            <Route path="filterbar" element={<PostFilterBar />} />
-            <Route path="noshowmodal" element={<NoshowModal />} />
-        </Routes>
-    </BrowserRouter>
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />} />
+      <Route
+        path="postlist"
+        element={
+          <PostList posts={postListTest.posts} userid={4} major={"컴공"} />
+        }
+      />
+      <Route path="post" element={<AllPostList />} />
+      <Route path="postdetail" element={<PostDetailContainer />} />
+      <Route
+        path="majorpostlist"
+        element={
+          <MajorPostList major={"컴공"} posts={postListTest.posts} userid={1} />
+        }
+      />
+      <Route path="chatting" element={<Chatting />} />
+      <Route path="mypage" element={<MypageContainer />} />
+      <Route path="posting" element={<Posting />} />
+      <Route path="noshow" element={<NoshowReport users={noShowEx.users} />} />
+      <Route path="filterbar" element={<PostFilterBar />} />
+      <Route path="noshowmodal" element={<NoshowModal />} />
+      <Route path="signup" element={<SignUp />} />
+    </Routes>
+  </BrowserRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
