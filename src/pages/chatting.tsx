b0ff608c9a403942f6f2cdffab00c02afb6c2864
jh-jin <devjin0122@gmail.com>
2022-12-15 12:42:45 +0900

    Feat: Chatting Page 제출본

--- src/pages/chatting.tsx
+++ src/pages/chatting.tsx
@@ -5,13 +5,43 @@ import { ArrowLeft, User, LogOut } from "react-feather";
 import { ChatHeader } from "../components/Chat/ChatHeader";
 import { ChatInput } from "../components/Chat/ChatInput";
 import { ChatContainer } from "../components/Chat/ChatContainer";
+import { type } from "os";
+import { MenuItem, Select } from "@mui/material";
+
+export interface IChatMessage {
+  type: "recv" | "send";
+  name: string;
+  text: string;
+  time: string;
+}
 
 export function Chatting() {
+  const [items, setItems] = React.useState<IChatMessage[]>([
+    {
+      type: "recv",
+      name: "사과도리|사회학과21",
+      text: "안녕하세요",
+      time: "오후 2시 3분",
+    },
+    {
+      type: "recv",
+      name: "사과도리|사회학과21",
+      text: "안녕하세요222",
+      time: "오후 2시 3분",
+    },
+    {
+      type: "send",
+      name: "붐바음악|음악학과21",
+      text: "안녕하세요333",
+      time: "오후 2시 3분",
+    },
+  ]);
+
   return (
     <div className={styles.ChattingWrapper}>
       <ChatHeader />
-      <ChatContainer />
-      <ChatInput />
+      <ChatContainer items={items} />
+      <ChatInput setItems={setItems} />
     </div>
   );
 }
